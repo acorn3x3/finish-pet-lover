98120695e4f7eb9b2dba6f522111ed8be342f7aa
Andrew Cornejo <atcornejo3@gmail.com>
2022-10-03 13:48:48 -0700

    inserted supabase url and key

--- pet-editor/pet-editor.js
+++ pet-editor/pet-editor.js
@@ -1,6 +1,7 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import '../auth/user.js';
+import { createPet, uploadImage } from '../fetch-utils.js';
 // > Part A: import upload image
 // > Part B: import fetch to create a pet
 
